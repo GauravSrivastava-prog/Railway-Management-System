97b4f2a48713bd6a69cf4cb4a57050199f03a141
Gaurav Srivastava <142695992+GauravSrivastava-prog@users.noreply.github.com>
2024-05-05 05:49:29 +0530

    Delete class-work directory

--- class-work/for2.py
+++
@@ -1,6 +0,0 @@
-i = 0
-arr=['Peter', 'John', 'David']
-print(len(arr))
-for i in range(len(arr)):
-    print(i)
-    print(arr[i]) 
--- class-work/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-print("First Dictionary : ")
-print(car)
-print("Second Dictionary : ")
-print(car2)
-print(car["Name"]) #Prints the value if the 'key' = name
-print("Length of car dictionary is : ",len(car))
